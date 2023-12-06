6786d790fbc33fd8e1b61dccdbc4d3b1cce3dae9
Raul Silva <raul.silva@kaxanuk.mx>
2023-12-06 08:56:13 -0600

    backtest de estrategias periodicas

--- market_timing_strat.py
+++ market_timing_strat.py
@@ -86,6 +86,7 @@ buy_signs2 = np.where(
 signs = sell_signs1 + sell_signs2 + buy_signs1 + buy_signs2
 
 df['strategy_sign'] = signs
+df['strategy_sign_lagg'] = df['strategy_sign'].shift()
 
 # %%
 
@@ -117,14 +118,14 @@ start = pd.to_datetime(df_spy.index[0])
 df = df[df.index >= start]
 
 # %%
-
-strat_df = df[['Phase', 'strategy_sign','strategy']]
+# 'strategy_sign'
+strat_df = df[['Phase', 'strategy_sign_lagg','strategy']]
 
 strat_df['stock_cum'] = 0
 
 #%%
 
-strat_df['buy_stocks'] = np.where((strat_df['strategy_sign'] == 1), 1, 0)
+strat_df['buy_stocks'] = np.where((strat_df['strategy_sign_lagg'] == 1), 4, 0)
 
 strat_df['total_stocks'] = strat_df['buy_stocks'].cumsum()
 
@@ -139,24 +140,26 @@ print(f'Compras {shares_per_year:.2f} por año en {((strat_df.index[-1]-strat_df
 
 #%%
 
-df_spy_daily = pd.concat([df_spy_daily['Adj Close'], strat_df[['buy_stocks','total_stocks', 'strategy_sign']]], axis = 1)
-df_spy_daily['total_stocks'] = df_spy_daily['total_stocks'].fillna(method = 'ffill')
-df_spy_daily['buy_stocks'] = df_spy_daily['buy_stocks'].fillna(0)
-df_spy_daily['strategy_sign'] = df_spy_daily['strategy_sign'].fillna(0)
+spy_strat = pd.concat([df_spy_daily['Adj Close'], strat_df[['buy_stocks','total_stocks', 'strategy_sign_lagg']]], axis = 1)
+spy_strat['total_stocks'] = spy_strat['total_stocks'].fillna(method = 'ffill')
+spy_strat['buy_stocks'] = spy_strat['buy_stocks'].fillna(0)
+spy_strat['strategy_sign_lagg'] = spy_strat['strategy_sign_lagg'].fillna(0)
 "1997-07-31"
-df_spy_daily['Adj Close'] = df_spy_daily['Adj Close'].fillna(method='bfill')
-df_spy_daily.dropna(inplace = True)
+spy_strat['Adj Close'] = spy_strat['Adj Close'].fillna(method='bfill')
+spy_strat.dropna(inplace = True)
 
-df_spy_daily['portfolio_value'] = df_spy_daily['total_stocks'] * df_spy_daily['Adj Close']
+spy_strat['portfolio_value'] = spy_strat['total_stocks'] * spy_strat['Adj Close']
 
 
 #%%
-initial_investment = df_spy_daily.loc[df_spy_daily['strategy_sign']==1, 'Adj Close'].sum()
-ending_value = df_spy_daily.iloc[-1, 4]
+portfolio_df = spy_strat.loc[spy_strat['strategy_sign_lagg']==1]
+initial_investment = (portfolio_df['buy_stocks'] * portfolio_df['Adj Close']).sum()
+
+ending_value = spy_strat.iloc[-1, 4]
 
 total_ret = ending_value/initial_investment
 
-an_ret = (total_ret ** (252/df_spy_daily.shape[0]))-1
+an_ret = (total_ret ** (252/spy_strat.shape[0]))-1
 
 print(f'total annual return: {an_ret: .4%}')
 
@@ -164,9 +167,13 @@ print(f'total annual return: {an_ret: .4%}')
 
 def string_formater(string):
     return str(string).split(' ')[0]
-buy_stock_dates = pd.bdate_range(start=string_formater(df_spy_daily.index[0]),
-                                end= string_formater(df_spy_daily.index[-1]),
-                                freq= 'AS')
+
+buy_stock_dates = pd.bdate_range(
+    start=string_formater(spy_strat.index[0]),
+    end= string_formater(spy_strat.index[-1]),
+    freq= 'AS'
+)
+
 len(buy_stock_dates)
 
 offset_dates = []
@@ -175,32 +182,30 @@ for date in buy_stock_dates:
     offset_dates.append(next_business_day)
 
 # offset_dates = [string_formater(i) for i in offset_dates]
-# buy_stock_dates = pd.date_range(start=string_formater(df_spy_daily.index[0]),
-#                                 end= string_formater(df_spy_daily.index[-1]),
+# buy_stock_dates = pd.date_range(start=string_formater(spy_strat.index[0]),
+#                                 end= string_formater(spy_strat.index[-1]),
 #                                 periods=10)
-df_spy_daily.loc[offset_dates, 'periodic_strat_signal'] = 1
-df_spy_daily['periodic_strat_signal'] = df_spy_daily['periodic_strat_signal'].fillna(0)
-df_spy_daily['periodic_strat_pf'] = df_spy_daily['periodic_strat_signal'].cumsum()
-df_spy_daily['periodic_strat_pf'] = df_spy_daily['periodic_strat_pf'].fillna(method='ffill')
 
-df_spy_daily['periodic_strat_portval'] = df_spy_daily['periodic_strat_pf'] * df_spy_daily['Adj Close']
+shares_periodic = 10
 
+spy_strat.loc[offset_dates, 'periodic_strat_signal'] = shares_periodic
+spy_strat['periodic_strat_signal'] = spy_strat['periodic_strat_signal'].fillna(0)
+spy_strat['periodic_strat_pf'] = spy_strat['periodic_strat_signal'].cumsum()
+spy_strat['periodic_strat_pf'] = spy_strat['periodic_strat_pf'].fillna(method='ffill')
 
+spy_strat['periodic_strat_portval'] = spy_strat['periodic_strat_pf'] * spy_strat['Adj Close']
 
-initial_investment_per = df_spy_daily.loc[df_spy_daily['periodic_strat_signal']==1, 'Adj Close'].sum()
-ending_value_per = df_spy_daily.iloc[-1, 7]
+per_spy = spy_strat.loc[spy_strat['periodic_strat_signal']==shares_periodic]
+initial_investment_per = (per_spy['Adj Close'] * per_spy['periodic_strat_signal']).sum()
+
+ending_value_per = spy_strat.iloc[-1, 7]
 
 total_ret_per = ending_value_per/initial_investment_per
 
-an_ret_per = (total_ret_per ** (252/df_spy_daily.shape[0]))-1
+an_ret_per = (total_ret_per ** (252/spy_strat.shape[0]))-1
 
 
-print(f'total annual return: {an_ret: .4%}')
-print(f'total annual return: {an_ret_per: .4%}')
-
-
-#%%
-
-
+print(f'Edgar Strat annual return: {an_ret: .4%}')
+print(f'Periodic Strat annual return: {an_ret_per: .4%}')
 
 
